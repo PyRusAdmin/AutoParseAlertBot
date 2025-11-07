eae7fb5c728a46fc7bafb903b7726ab52be638c1
pyadminru <zh.vitaliy92@yandex.ru>
2025-11-07 15:51:47 +0300

    Добавлено автоматическое создание .env файла.

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+
+os.system("""pip install -r requirements.txt""")
+
+# Создание .env файла, если он не существует
+env_example = """
+BOT_TOKEN=_____
+ID=_____
+HASH=_____
+""".strip()
+
+env_file = ".env"
+
+if not os.path.exists(env_file):
+    with open(env_file, "w", encoding="utf-8") as f:
+        f.write(env_example)
+    print(f"✅ Файл '{env_file}' успешно создан.")
+else:
+    print(f"ℹ️ Файл '{env_file}' уже существует.")
