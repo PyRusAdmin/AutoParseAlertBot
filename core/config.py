d4908cc611d85e00828c1cfc8b13373880859ee8
pyadminru <zh.vitaliy92@yandex.ru>
2025-12-23 23:44:57 +0300

    Рефакторинг кода

--- core/config.py
+++ core/config.py
@@ -18,10 +18,6 @@ def read_config_file():
 
 config = read_config_file()  # Чтение конфигурации из config.ini
 username = config['telegram_settings']['username']
-selectedmodel = config['Settings']['selectedmodel']
-number_of_groups = config['ai']['number_of_groups']
-program_version = config['program_version']['program_version']
-date_of_program_change = config['date_of_program_change']['date_of_program_change']
 
 GROQ_API_KEY = config['API_Groq']['GROQ_API_KEY']
 
