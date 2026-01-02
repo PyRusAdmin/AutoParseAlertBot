f061daf0c3a24048dd5e5a58f9500a22253fa371
pyadminru <zh.vitaliy92@yandex.ru>
2026-01-02 21:47:28 +0300

    Формирование документации

--- handlers/entering_keyword.py
+++ handlers/entering_keyword.py
@@ -22,12 +22,9 @@ async def handle_enter_keyword_menu(message: Message, state: FSMContext):
 
     Переводит пользователя в состояние ожидания ввода (MyStates.entering_keyword).
 
-    Args:
-        message (Message): Объект входящего сообщения от пользователя.
-        state (FSMContext): Контекст машины состояний, используется для сброса и установки состояния.
-
-    Returns:
-        None
+    :param message: (Message) Объект входящего сообщения от пользователя.
+    :param state: (FSMContext) Контекст машины состояний, используется для сброса и установки состояния.
+    :return: None
     """
     await state.clear()  # Завершаем текущее состояние машины состояния
     telegram_user = message.from_user
