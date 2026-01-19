ceba92ce9d9df21ec643a06d407f5033ad1424e5
pyadminru <zh.vitaliy92@yandex.ru>
2026-01-19 15:08:31 +0300

    Формирование функции "Проверка группы на наличие ключевых слов"

--- handlers/user/checking_group_for_keywords.py
+++ handlers/user/checking_group_for_keywords.py
@@ -2,7 +2,7 @@
 from aiogram import F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
-
+from loguru import logger
 from keyboards.keyboards import back_keyboard
 from states.states import MyStatesParsing
 from system.dispatcher import router
@@ -20,8 +20,7 @@ async def checking_group_for_keywords(message: Message, state: FSMContext):
     :return: None
     """
     await state.clear()  # Завершаем текущее состояние машины состояния
-    user_tg = message.from_user
-
+    # user_tg = message.from_user
     text = "Введите ссылку на группу, для поиска ключевых слов"
     await message.answer(text, reply_markup=back_keyboard())
 
@@ -30,8 +29,8 @@ async def checking_group_for_keywords(message: Message, state: FSMContext):
 
 @router.message(MyStatesParsing.get_url)
 async def get_url(message: Message, state: FSMContext):
-    raw_input = message.text.strip()
-
+    # raw_input = message.text.strip()
+    await state.update_data(url=message.text.strip())  # Сохраняем URL в контекст данных
     text = "Введите ключевое слово для поиска\n\n"
     await message.answer(text, reply_markup=back_keyboard())
 
@@ -40,12 +39,14 @@ async def get_url(message: Message, state: FSMContext):
 
 @router.message(MyStatesParsing.get_keyword)
 async def get_keyword(message: Message, state: FSMContext):
-    raw_input = message.text.strip()
-
+    keyword = message.text.strip()
     text = "Данные приняты, ожидайте результата\n\n"
     await message.answer(text, reply_markup=back_keyboard())
-
+    await state.update_data(keyword=keyword)
+    data = await state.get_data()  # Получаем данные из контекста состояния
     await state.clear()  # Завершаем текущее состояние машины состояния
+
+    logger.info(f"Полученые данные от пользователя: ссылка {data.get("url")}, ключевое слово: {data.get('keyword')}")
 
 
 def register_handlers_checking_group_for_keywords():
